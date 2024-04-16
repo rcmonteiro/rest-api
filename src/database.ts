d650cc31a31f8580fb3b964107a517f115237e61
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-16 14:08:32 -0300

    Prepare to deploy on Render

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import { knex, Knex } from 'knex'
 import { env } from './env'
 
 export const knexOptions: Knex.Config = {
-  client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite3'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
