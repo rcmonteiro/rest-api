54d605df9ce3ab52fc836e4637c7978bce45361e
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-16 09:36:49 -0300

    Install Knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { db } from './database'
 
 const app = fastify()
 
-app.get("/", () => {
-  return "Hello, World!"
+app.get('/', async () => {
+  const tables = await db('sqlite_schema').select('*')
+  return tables
 })
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Server running on port 3333")
+  console.log('Server running on port 3333')
 })
