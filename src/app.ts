4cd4c10333319acf797d14616ad8129bf5df0896
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-16 13:31:55 -0300

    Testes e2e com vitest

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import fastifyCookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { transactionsRoutes } from './routes/transactions'
+
+export const app = fastify()
+
+app.register(fastifyCookie)
+app.register(transactionsRoutes, { prefix: 'transactions' })
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import fastifyCookie from '@fastify/cookie'
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-app.register(fastifyCookie)
-app.register(transactionsRoutes, { prefix: 'transactions' })
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`[${env.NODE_ENV}] Server running on port ${env.PORT}`)
